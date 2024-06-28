c33c468752a12a014eb6cb7200a029bc88b74773
Cho Chan Woo <ccw040607@gmail.com>
2024-06-28 19:10:45 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,80 @@
+from flask import Flask, render_template
+import requests, datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+from info import user_list
+
+app = Flask(__name__)
+DATE = ''
+
+problem_list = {}
+class Problem:
+    def __init__(self, problem_id, title, level):
+        self.problem_id = str(problem_id)
+        self.title = title
+        self.level = str(level)
+        self.img_src = str(level) + '.svg'
+        self.alt_level = 'BSGP'[(level-1) // 5] + str(5 - level % 5)
+        self.url = 'https://www.acmicpc.net/problem/' + str(problem_id)
+    
+    def __str__(self):
+        return f'{self.level} {self.problem_id} {self.title}'
+    
+    def __repr__(self):
+        return str(self)
+
+def set_problem():
+    global DATE, problem_list
+
+    ret = {}
+    url = "https://solved.ac/api/v3/search/problem"
+    now_user = []
+
+    for level in user_list:
+        now_user += user_list[level]
+        query = f'*{level[0].lower()} %ko -(@{"|@".join(now_user)})'
+        querystring = {"query":query, "sort":"random"}
+        headers = {
+            "x-solvedac-language": "ko",
+            "Accept": "application/json"
+        }
+
+        response = requests.get(url, headers=headers, params=querystring)
+
+        problems = response.json()['items']
+        if len(problems) < 3:
+            ret.append([])
+        else:
+            now_problem = []
+            for i in range(3):
+                now_problem.append(Problem(
+                    problem_id=problems[i]["problemId"], 
+                    title=problems[i]["titleKo"], 
+                    level=problems[i]["level"]
+                ))
+            ret[level] = now_problem
+            
+    problem_list = ret
+
+    t = datetime.datetime.now()
+    WEEKDAY = {0:'월', 1:'화', 2:'수', 3:'목', 4:'금', 5:'토', 6:'일'}
+    DATE = f'{t.year}년 {t.month}월 {t.day}일 ({WEEKDAY[t.weekday()]})'
+    # DATE = t.strftime('%m/%d %H:%M')
+
+    # print("problem set: ")
+    # print(problem_list)
+    # print(DATE)
+    
+
+@app.route('/')
+def root():
+    global DATE, problem_list
+    return render_template('main.html', update_time = DATE, problem_list = problem_list)
+
+if __name__ == '__main__':
+    with app.app_context():
+        if not problem_list:
+            set_problem()
+    worker = BackgroundScheduler(timezone='Asia/Seoul')
+    worker.add_job(set_problem, 'cron', hour=6)
+    worker.start()
+    app.run()
